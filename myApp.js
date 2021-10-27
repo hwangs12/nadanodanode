3adafe397cc116875245ba92eb968d9886d01730
hwangs12 <sungjun94@gmail.com>
2021-10-26 20:35:00 -0400

    adding dotenv

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 var express = require("express");
 var app = express();
 
@@ -8,6 +9,15 @@ app.use("/public", express.static(public));
 
 app.get("/", (req, res) => {
 	res.sendFile(absolutePath);
+});
+
+app.get("/json", (req, res) => {
+	if (process.env.MESSAGE_STYLE === "uppercase") {
+		const uppercase = "Hello json".toUpperCase();
+		res.json({ message: uppercase });
+	} else {
+		res.status(500).send("Something broke!");
+	}
 });
 
 module.exports = app;
