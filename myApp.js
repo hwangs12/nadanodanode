74d05818aedb42f11bd3135891b2d4c2f5b4f396
hwangs12 <sungjun94@gmail.com>
2021-10-27 18:13:39 -0400

    post requst and make it show on browser

--- myApp.js
+++ myApp.js
@@ -1,11 +1,17 @@
 require("dotenv").config();
+var bodyParser = require("body-parser");
 var express = require("express");
 var app = express();
 
 let absolutePath = __dirname + "/views/index.html";
 let public = __dirname + "/public";
+let uppercase;
 
 app.use("/public", express.static(public));
+app.use((req, res, next) => {
+	console.log(req.method, req.path, "-", req.ip);
+	next();
+}, bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
 	res.sendFile(absolutePath);
@@ -13,11 +19,37 @@ app.get("/", (req, res) => {
 
 app.get("/json", (req, res) => {
 	if (process.env.MESSAGE_STYLE === "uppercase") {
-		const uppercase = "Hello json".toUpperCase();
-		res.json({ message: uppercase });
+		uppercase = "Hello json".toUpperCase();
 	} else {
-		res.status(500).send("Something broke!");
+		uppercase = "Hello json";
 	}
+	res.json({ message: uppercase });
 });
+
+app.get(
+	"/now",
+	(req, res, next) => {
+		req.time = new Date().toString();
+		next();
+	},
+	(req, res) => {
+		res.json({ time: req.time });
+	}
+);
+
+app.get("/:word/echo", (req, res) => {
+	const word = req.params.word;
+	res.json({ echo: word });
+});
+
+app.route("/name")
+	.get((req, res) => {
+		const { first, last } = req.query;
+		res.json({ name: `${first} ${last}` });
+	})
+	.post((req, res) => {
+		const { first, last } = req.body;
+		res.json({ name: `${first} ${last}` });
+	});
 
 module.exports = app;
